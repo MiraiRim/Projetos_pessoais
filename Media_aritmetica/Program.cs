0c986fa7b563671100de1e7900d86d1116fe5aed
unknown <fabianasantosdejesus@gmail.com>
2021-10-27 15:29:30 -0300

    Aplicação console em C# para obter a média aritmética

---
+++ Media_aritmetica/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Media_aritmetica
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            
+            int nota1, nota2, nota3, nota4, media;
+            Console.WriteLine("Informe a nota 1");
+            nota1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe a nota 2");
+            nota2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe a nota 3");
+            nota3 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe a nota 4");
+            nota4 = int.Parse(Console.ReadLine());
+
+            media = (nota1 + nota2 + nota3 + nota4) / 4;
+            if (media >= 6)
+            {
+                Console.WriteLine("Parabêns você foi aprovado!!! Sua média é:");
+                Console.WriteLine(media);
+            }
+            if (media < 6)
+            {
+                Console.WriteLine("Infelizmente você foi repovado... Sua média é:");
+                Console.WriteLine(media);
+            }
+
+        }
+    }
+}
